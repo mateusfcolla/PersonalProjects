9dc8771edd846749a8f2deb68a2287223a584857
Mateus Felipe <45042839+mateusfcolla@users.noreply.github.com>
2019-09-17 10:57:12 -0300

    Added love.ejs and star.ejs

--- MoreExpress/EJSDemo/app.js
+++ MoreExpress/EJSDemo/app.js
@@ -5,6 +5,15 @@ app.get('/', function(req, res){
     res.render("index.ejs");
 });
 
+app.get('/fallinlovewith/:thing', function(req, res){
+    var thing = req.params.thing;
+    res.render('love.ejs', {thing : thing});
+});
+
+app.get('*', function(req, res){
+    res.render('star.ejs');
+});
+
 app.listen(1808, function(){
     console.log('Server online!');
 });
