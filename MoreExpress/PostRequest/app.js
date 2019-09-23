73e424129a022fccb82464e26c3df659682d690c
Mateus Felipe <45042839+mateusfcolla@users.noreply.github.com>
2019-09-23 12:54:56 -0300

    PostRequest(1)Added

--- MoreExpress/PostRequest/app.js
+++ MoreExpress/PostRequest/app.js
@@ -1,8 +1,20 @@
 var express = require('express');
 var app = express();
+
 app.set('view engine', 'ejs');
 
+app.get('/', function(req, res){
+    res.render('home');
+});
 
+app.post('/addfriend', function(req, res){
+    res.send("You've reached the post route!");
+});
+
+app.get('/friends', function(req, res){
+    var friends = ['Jeff', 'Miranda', 'Justin', 'Pierre', 'Lilly']
+    res.render('friends', {friends:friends});
+});
 
 app.listen(1808, function(){
     console.log('Server online!');
