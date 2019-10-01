597136e428ff4707369835a8a1e01fc3a0a50e6f
Mateus Felipe <45042839+mateusfcolla@users.noreply.github.com>
2019-10-01 15:21:47 -0300

    Added Project

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -13,8 +13,9 @@ app.get('/campgrounds', function(req, res){
         {name: 'Shrek Shrek', image: 'https://i.kym-cdn.com/photos/images/original/001/271/550/8b6.jpg'},
         {name: 'Puss in Boots', image: 'https://i.kym-cdn.com/photos/images/original/001/271/550/8b6.jpg'}
     ];
+    res.render('campgrounds')
 });
 
 app.listen(1808, function(){
     console.log('YelpCamp Server Has Started!');
-})
+});
