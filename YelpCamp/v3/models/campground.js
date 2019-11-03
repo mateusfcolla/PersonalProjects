2ff02a51110946f3bdee06c0d4a7a20577d49153
Mateus Felipe <45042839+mateusfcolla@users.noreply.github.com>
2019-11-03 15:46:50 -0300

    Added v3 file and added Models folder

---
+++ YelpCamp/v3/models/campground.js
@@ -0,0 +1,7 @@
+const campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String,
+    description: String
+});
+
+module.exports = mongoose.model("Campground", campgroundSchema);
