86dec3a430d35d27069a365a031b12c3542a08f5
Mateus Felipe <45042839+mateusfcolla@users.noreply.github.com>
2019-11-05 10:57:41 -0300

    Added landing page into Authentication

---
+++ Authentication/app.js
@@ -0,0 +1,23 @@
+const express               = require('express'),
+      mongoose              = require('mongoose'),
+      passport              = require('passport'),
+      bodyParser            = require('body-parser'),
+      LocalStrategy         = require('passport-local'),
+      passportLocalMongoose = require('passport-local-mongoose'),
+      methodOverride        = require('method-override'),
+      User                  = require('./models/user'),
+      app                   = express();
+
+mongoose.connect('mongodb://localhost:27017/auth_demo_app', {useFindAndModify: false, useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true});app.use(bodyParser.urlencoded({extended: true}));
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride('_method'));
+
+app.get('/', (req, res)=>{
+    res.render('landing');
+});
+
+app.listen(3000, ()=>{
+    console.log('Server started!');
+})
