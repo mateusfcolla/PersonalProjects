19136fc26c78d9c2f97a02728bcd5c4eeca12d28
Mateus Felipe <45042839+mateusfcolla@users.noreply.github.com>
2019-11-05 16:00:33 -0300

    Fixed problems and added middleware into Authentication

--- Authentication/app.js
+++ Authentication/app.js
@@ -2,20 +2,19 @@ const express               = require('express'),
       mongoose              = require('mongoose'),
       passport              = require('passport'),
       bodyParser            = require('body-parser'),
+      User                  = require('./models/user'),
       LocalStrategy         = require('passport-local'),
       passportLocalMongoose = require('passport-local-mongoose'),
       methodOverride        = require('method-override'),
-      User                  = require('./models/user'),
       app                   = express();
 
-mongoose.connect('mongodb://localhost:27017/auth_demo_app', {useFindAndModify: false, useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true});app.use(bodyParser.urlencoded({extended: true}));
+mongoose.connect('mongodb://localhost:27017/auth_demo_app', {useFindAndModify: false, useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true});
 app.set('view engine', 'ejs');
-app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(methodOverride('_method'));
+app.use(require('express-session')({ secret: 'Ma nam jef', resave: false, saveUninitialized: false }));
 app.use(passport.initialize());
 app.use(passport.session());
-app.use(require('express-session')({ secret: 'Ma nam jef', resave: false, saveUninitialized: false }));
 passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
@@ -26,7 +25,7 @@ app.get('/', (req, res)=>{
 });
 
 // Secret page route
-app.get('/secret', (req, res) =>{
+app.get('/secret', isLoggedIn , (req, res) =>{
     res.render('secret');
 });
 
@@ -39,7 +38,7 @@ app.get('/register', (req, res)=>{
 // User sign-up
 app.post('/register', (req, res)=>{
     User.register(new User({username: req.body.username}), req.body.password, (err, newUser)=>{
-        if(err){console.log(err); res.render('user/register')};
+        if(err){console.log(err); return res.render('user/register')};
         passport.authenticate('local')(req, res, ()=>{
             res.redirect('/secret');
         });
@@ -50,10 +49,25 @@ app.get('/login', (req, res)=>{
     res.render('user/login');
 })
 // Login logic
-app.post('/login', passport.authenticate('local', { successRedirect: '/secret', failureRedirect: '/login'}) , (req, res)=>{
-    
+app.post('/login', passport.authenticate('local', {
+     successRedirect: '/secret',
+     failureRedirect: '/login'
+    }) , (req, res)=>{
 });
+
+// LOGOUT ROUTES
+app.get('/logout', (req, res)=>{
+    req.logout();
+    res.redirect('/');
+});
+
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect('/login');
+}
 
 app.listen(3000, ()=>{
     console.log('Server started!');
-})
+});
